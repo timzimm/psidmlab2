d43b000922d4d0f4935b12c39a6750c8be6bda57
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-03-16 18:09:13 +0100

    add a(tau) debug section

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "common.h"
+#include "cosmology.h"
 #include "io.h"
 
 int main(int argc, char** argv) {
@@ -8,20 +9,47 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    Parameters parameters(argv[1]);
-    std::cout << parameters;
+    // Populate Parameter struct
+    Parameters param(argv[1]);
+
+    // Initialize the cosmological model, i.e. setup the relation
+    // a(tau) and tau(a)
+    Cosmology cosmo(param);
+
+    // At this point tau_end is set for the static case as specified in the ini
+    // file. This might be wrong, so let's check.
+    if (param.cosmo != Parameters::Model::Static)
+        param.tau_end = cosmo.tau_of_a(param.a_end);
+
+    // Parameters are now correctly initialized. Print them.
+    std::cout << param;
+
+    // Setup HDF5 file
+    OutputFile file(param);
+
+#ifndef NDEBUG
+    std::cout << INFOTAG("Saving a(tau) to file") << std::endl;
+    d_vector tau_grid, a_values;
+    double tau = param.tau_start;
+    while (tau < param.tau_end) {
+        tau_grid.push_back(tau);
+        a_values.push_back(cosmo.a_of_tau(tau));
+        tau += param.dtau;
+    }
+    file.write(tau_grid, "tau");
+    file.write(a_values, "a_of_tau");
+#endif
 
     // Dummy potential
     SimState state;
-    c_vector psis(parameters.M * parameters.N, std::complex<double>(1, 1));
-    d_vector Vs(parameters.M * parameters.N, 0.0);
+    c_vector psis(param.M * param.N, std::complex<double>(1, 1));
+    d_vector Vs(param.M * param.N, 0.0);
 
     state.Vs = Vs;
     state.psis = psis;
     state.n = 1;
     state.tau = 42.2;
 
-    OutputFile file_hdf5(parameters);
-    file_hdf5.write(state, parameters);
+    file.write(state, param);
     return 0;
 }
