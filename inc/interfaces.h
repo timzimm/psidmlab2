0cb0c0f6227725131cd5bb5e34f5e52b41e2108d
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-04-14 16:05:08 +0200

    add abstract interface

--- inc/interfaces.h
+++ inc/interfaces.h
@@ -5,7 +5,7 @@
 // This header defines all interfaces of
 //
 // SchroedingerMethod (like Schroedinger::USO_DKD, Schroedinger::CN, ...)
-// PotentialMethod (like Poisson::FFT, Wave::LAX, ...)
+// PotentialMethod (like Poisson::FFT, Poisson::FD, ...)
 //
 // The interface is currently as generic as possible and might change in the
 // future to something more expressive/functional.
@@ -15,8 +15,6 @@
 struct SimState;
 #include "parameters_fwd.h"
 
-// Note that a PotentialMethod can be based on a Stepper (wave equation)
-// or a solver (poisson equation).
 class PotentialMethod : public Factory<PotentialMethod, Parameters> {
    public:
     virtual void solve(SimState& state) = 0;
@@ -27,5 +25,11 @@ class SchroedingerMethod : public Factory<SchroedingerMethod, Parameters> {
    public:
     virtual void step(SimState& state) = 0;
     virtual ~SchroedingerMethod() = default;
+};
+
+class Observable : public Factory<Observable, Parameters> {
+   public:
+    virtual void compute(SimState& state) = 0;
+    virtual ~Observable() = default;
 };
 #endif
