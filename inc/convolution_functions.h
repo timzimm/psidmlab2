ff21aa6fdb0313a2e596350258477ddc825ecc9d
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-04-19 13:30:58 +0200

    add include guard and comments

--- inc/convolution_functions.h
+++ inc/convolution_functions.h
@@ -1,3 +1,6 @@
+#ifndef __CONVOLUTION__
+#define __CONVOLUTION__
+
 #include <blaze/math/DynamicVector.h>
 #include <fftw3.h>
 #include <cassert>
@@ -30,24 +33,41 @@ struct convolution_ws {
     ~convolution_ws();
 };
 
+// Applies discrete (linear/circular) convolution by calling one of the routines
+// below depending on the threshold value. If N_kernel > threshold DFT based
+// fast convolution is used (complexity O(PlogP)). Otherwise the direct
+// summation algorithm is used (complexity O(N_signal*N_kernel)). Note that the
+// latter is faster for small kernels.
 void discrete_convolution(convolution_ws& data,
                           const blaze::DynamicVector<double>& kernel,
                           const blaze::DynamicVector<double>& signal);
+
+// Applies DFT convolution (linear/circular) on signal with kernel using
+// allocated memeory in ws. If data.computeLinear is false, the shift theorem is
+// applied to recenter the output data. This might change in the future.
 void fast_convolution(convolution_ws& data,
                       const blaze::DynamicVector<double>& kernel,
                       const blaze::DynamicVector<double>& signal);
 
+// Applies sum based linear convolution of signal with kernel and recenters its
+// output data.signal_padded on the fly.
 void linear_convolution_sum(convolution_ws& data,
                             const blaze::DynamicVector<double>& kernel,
                             const blaze::DynamicVector<double>& signal);
 
+// Applies sum based circular convolution of signal with kernel.
 void circular_convolution_sum(convolution_ws& data,
                               const blaze::DynamicVector<double>& kernel,
                               const blaze::DynamicVector<double>& signal);
 
+// Taken from @jeremyfix who took it from GSL. These routines compute the
+// optimal padding values to achieve maximum performance of FFTW for DFT
+// convolutions.
 void factorize(const int n, int* n_factors, int factors[],
                int* implemented_factors);
 
 bool is_optimal(int n, int* implemented_factors);
 
 int find_closest_factor(int n);
+
+#endif
