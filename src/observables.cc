35821dc5863079524e3ed9e08e0c60d4cd549819
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-04-25 15:54:38 +0200

    completed wigner implementation

--- src/observables.cc
+++ src/observables.cc
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include "blaze/math/Columns.h"
 #include "blaze/math/Elements.h"
 #include "blaze/math/Rows.h"
 #include "blaze/math/Submatrix.h"
@@ -24,14 +25,17 @@ DensityContrast::DensityContrast(const Parameters& p)
       N_kernel(2 * floor(5 * sigma_x / dx) + 1),
       ws(husimi ? p["Analysis"]["linear_convolution"].get<bool>() : 0,
          husimi ? N : 0, husimi ? N_kernel : 0),
-      gaussian_kernel(N_kernel) {
-    // Kernel construction in x-space
-    for (int i = 0; i < N_kernel; ++i) {
-        gaussian_kernel[i] = (i - N_kernel / 2) * dx;
+      gaussian_kernel(0) {
+    if (husimi) {
+        gaussian_kernel.resize(N_kernel);
+        // Kernel construction in x-space
+        for (int i = 0; i < N_kernel; ++i) {
+            gaussian_kernel[i] = (i - N_kernel / 2) * dx;
+        }
+        gaussian_kernel =
+            exp(-0.5 / (sigma_x * sigma_x) * gaussian_kernel * gaussian_kernel);
+        gaussian_kernel /= blaze::sum(gaussian_kernel);
     }
-    gaussian_kernel =
-        exp(-0.5 / (sigma_x * sigma_x) * gaussian_kernel * gaussian_kernel);
-    gaussian_kernel /= blaze::sum(gaussian_kernel);
 }
 
 blaze::DynamicMatrix<double, blaze::columnMajor> DensityContrast::compute(
@@ -60,8 +64,17 @@ PhaseSpaceDistribution::PhaseSpaceDistribution(const Parameters& p)
 
 blaze::DynamicMatrix<double, blaze::columnMajor>
 PhaseSpaceDistribution::compute(const SimState& state) {
-    blaze::DynamicVector<std::complex<double>> chirp(N);
     using namespace blaze;
+    blaze::DynamicVector<std::complex<double>> chirp(N);
+
+    // Plan init is negligible compared to workload
+    DynamicVector<std::complex<double>> lag(N);
+    DynamicMatrix<double, columnMajor> W(2 * N, 2 * N);
+    auto lag_ptr = reinterpret_cast<fftw_complex*>(lag.data());
+
+    auto even_plan = fftw_plan_dft_c2r_1d(N, lag_ptr, W.data(), FFTW_ESTIMATE);
+    auto odd_plan =
+        fftw_plan_dft_1d(N, lag_ptr, lag_ptr, FFTW_FORWARD, FFTW_ESTIMATE);
 
     // Load chirp data
     std::ifstream input("/tmp/chirp/chirp.txt");
@@ -70,25 +83,25 @@ PhaseSpaceDistribution::compute(const SimState& state) {
 
     auto& signal = chirp;
 
-    DynamicMatrix<double, columnMajor> W(2 * N, 2 * N);
-
     // We only compute the first quadrant of W. All other quadrants follow by
     // symmetry properties of W
-    auto W_00 = blaze::submatrix(W, 0, 0, N, N);
+    auto W_00 = submatrix(W, 0, 0, N, N);
 
-    DynamicVector<std::complex<double>, blaze::rowVector> strip(N);
+    // Create Wigner distribution column by column.
 
-    auto in_out = reinterpret_cast<fftw_complex*>(strip.data());
-
-    fftw_plan one =
-        fftw_plan_dft_1d(N, in_out, in_out, FFTW_FORWARD, FFTW_ESTIMATE);
-
-    // First we create the "lag matrix" given by
-    //
-    // W(k, 2*n) = 1/(2N) * signal[(k+n)modN]signal[(n-k)modN]^*
-    // W(k, 2*n+1) = 1/(2N) * signal[(k+n)modN]signal[(n-k+1)modN]^*
+    // TODO: We could, in principle, first construct the entire lag matrix
+    // and then transform it once via FFTWs advanced interface. This would
+    // increase preformance (potentially significantly). The downside is that we
+    // either have to store two matrices to do the out of place FFT, or have one
+    // padded matrix for which we have to mess with C++ type system to
+    // accomodate std::complex and doubles at once. I don't know if this is
+    // really the right approach and worth the effort.
 
     for (int n = 0; 2 * n < N; ++n) {
+        // First we create the "lag matrix" column given by
+        // W(k, 2*n) = 1/(2N) * signal[(k+n)modN]signal[(n-k)modN]^*
+        // W(k, 2*n+1) = 1/(2N) * signal[(k+n)modN]signal[(n-k+1)modN]^*
+        // Only works for power of 2 N's !!!
         auto sig_npk =
             elements(signal, [&](size_t k) { return (n + k) % N; }, N);
         auto sig_nmk =
@@ -96,39 +109,57 @@ PhaseSpaceDistribution::compute(const SimState& state) {
         auto sig_nmk1 =
             elements(signal, [&](size_t k) { return (n - k + 1) % N; }, N);
 
-        strip = 1.0 / (2 * N) * sig_npk * conj(sig_nmk);
-        fftw_execute(one);
-        column(W_00, 2 * n) = real(strip);
-        strip = 1.0 / (2 * N) * sig_npk * conj(sig_nmk1);
-        fftw_execute(one);
+        // Even columns
+        // conj switched due to sign convention of FFTW. Result is real so no
+        // additional conj required.
+        lag = 1.0 / (2 * N) * conj(sig_npk) * sig_nmk;
+
+        fftw_execute_dft_c2r(even_plan, lag_ptr,
+                             W.data() + 2 * n * W.spacing());
+
+        // Odd columns
+        lag = 1.0 / (2 * N) * sig_npk * conj(sig_nmk1);
+
+        // TODO Maybe Toole's trick can be applied here. Check that
+        fftw_execute(odd_plan);
+
+        // TODO Benchmark with blaze::for_each
         for (int m = 0; m < N; ++m)
-            strip[m] *= std::exp(std::complex<double>(0, 1) * (M_PI * m / N));
-        column(W_00, 2 * n + 1) = real(strip);
+            lag[m] *= std::exp(std::complex<double>(0, 1) * (M_PI * m / N));
+
+        column(W_00, 2 * n + 1) = real(lag);
     }
 
     // W(k,n+N) = (-1)^k W(k,n)
-    auto W_01 = submatrix(W, 0, N, N, N) = W_00;
+    auto W_01 = submatrix(W, 0, N, N, N);
     for (int n = 0; n < N; ++n) {
-        for (int k = 0; 2 * k < N; ++k) {
-            W_01(2 * k + 1, n) *= -1;
+        int phase = 1;
+        for (int k = 0; k < N; ++k) {
+            W_01(k, n) = phase * W_00(k, n);
+            phase *= -1;
         }
     }
 
     // W(k+N,n) = (-1)^n W(k,n)
-    auto W_10 = submatrix(W, N, 0, N, N) = W_00;
+    auto W_10 = submatrix(W, N, 0, N, N);
     for (int n = 0; 2 * n < N; ++n) {
-        for (int k = 0; 2 * k < N; ++k) {
-            W_10(k, 2 * n + 1) *= -1;
-        }
+        column(W_10, 2 * n) = column(W_00, 2 * n);
+        column(W_10, 2 * n + 1) = -1 * column(W_00, 2 * n + 1);
     }
 
     // W(k+N,n+N) = (-1)^(k+n+N) W(k,n)
-    auto W_11 = submatrix(W, N, N, N, N) = W_00;
+    int phase = (N % 2 == 0) ? 1 : -1;
+    auto W_11 = submatrix(W, N, N, N, N);
     for (int n = 0; n < N; ++n) {
         for (int k = 0; k < N; ++k) {
-            W_11(k, n) *= std::pow(-1, k + n + N);
+            W(k + N, n + N) = W(k, n) * phase;
+            phase *= -1;
         }
+        phase *= -1;
     }
+
+    fftw_destroy_plan(odd_plan);
+    fftw_destroy_plan(even_plan);
 
     return W;
 }
