c36984d3080eba02660a8130ee4590a846b5d52d
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-05-21 16:45:50 +0200

    new wigner implmentation closer to pytftb

--- src/observables.cc
+++ src/observables.cc
@@ -61,8 +61,10 @@ PhaseSpaceDistribution::PhaseSpaceDistribution(const Parameters& p)
       N_kernel(2 * floor(5 * sigma_x / dx) + 1),
       t_prev(-1),
       ws(husimi ? linear : 0, husimi ? N : 0, husimi ? N_kernel : 0),
-      f(husimi ? 2 * N : N, husimi ? 2 * N : N, 0),
-      gaussian_kernel(0) {
+      gaussian_kernel(0),
+      f(N, N, 0),
+      iaf(N, N, 0),
+      c2c(nullptr) {
     if (husimi) {
         gaussian_kernel.resize(N_kernel);
         // Kernel construction in x-space
@@ -73,99 +75,38 @@ PhaseSpaceDistribution::PhaseSpaceDistribution(const Parameters& p)
             exp(-0.5 / (sigma_x * sigma_x) * gaussian_kernel * gaussian_kernel);
         gaussian_kernel /= blaze::sum(gaussian_kernel);
     }
+    auto iaf_ptr = reinterpret_cast<fftw_complex*>(iaf.data());
+    c2c = fftw_plan_many_dft(1, &N, N, iaf_ptr, nullptr, 1, iaf.spacing(),
+                             iaf_ptr, nullptr, 1, iaf.spacing(), FFTW_FORWARD,
+                             FFTW_ESTIMATE);
 }
 
 void PhaseSpaceDistribution::wigner_distribution(const SimState& state) {
     using namespace blaze;
-
-    DynamicVector<std::complex<double>> lag(N);
-    auto lag_ptr = reinterpret_cast<fftw_complex*>(lag.data());
-
-    auto even_plan = fftw_plan_dft_c2r_1d(N, lag_ptr, f.data(), FFTW_ESTIMATE);
-    auto odd_plan =
-        fftw_plan_dft_1d(N, lag_ptr, lag_ptr, FFTW_FORWARD, FFTW_ESTIMATE);
-
-    // We only compute the first quadrant of W. All other quadrants follow
-    // by symmetry properties of W
-    auto W_00 = submatrix(f, 0, 0, N, N);
-
     for (int m = 0; m < state.M; ++m) {
         auto psi = column(state.psis, m);
-        // Create Wigner distribution column by column.
-
-        // TODO: We could, in principle, first construct the entire lag
-        // matrix and then transform it once via FFTWs advanced interface.
-        // This would increase preformance (potentially significantly). The
-        // downside is that we either have to store two matrices to do the
-        // out of place FFT, or have one padded matrix for which we have to
-        // mess with C++ type system to accomodate std::complex and doubles
-        // at once. I don't know if this is really the right approach and
-        // worth the effort.
-
-        for (int n = 0; 2 * n < N; ++n) {
-            // First we create the "lag matrix" column given by
-            // W(k, 2*n) = 1/(2N) * psi[(k+n)modN]psi[(n-k)modN]^*
-            // W(k, 2*n+1) = 1/(2N) * psi[(k+n)modN]psi[(n-k+1)modN]^*
-            // Only works for power of 2 N's !!!
-            auto sig_npk =
-                elements(psi, [&](size_t k) { return (n + k) % N; }, N);
-            auto sig_nmk =
-                elements(psi, [&](size_t k) { return (n - k) % N; }, N);
-            auto sig_nmk1 =
-                elements(psi, [&](size_t k) { return (n - k + 1) % N; }, N);
-
-            // Even columns
-            // conj switched due to sign convention of FFTW. Result is real
-            // so no additional conj required.
-            lag = state.lambda[m] / (2 * N) * conj(sig_npk) * sig_nmk;
-
-            fftw_execute_dft_c2r(even_plan, lag_ptr,
-                                 f.data() + 2 * n * f.spacing());
-
-            // Odd columns
-            lag = state.lambda[m] / (2 * N) * sig_npk * conj(sig_nmk1);
-
-            // TODO Maybe Toole's trick can be applied here. Check that
-            fftw_execute(odd_plan);
-
-            for (int m = 0; m < N; ++m)
-                lag[m] *= std::exp(std::complex<double>(0, 1) * (M_PI * m / N));
-
-            column(W_00, 2 * n + 1) = real(lag);
-        }
-
-        // W(k,n+N) = (-1)^k W(k,n)
-        auto W_01 = submatrix(f, 0, N, N, N);
-        for (int n = 0; n < N; ++n) {
-            int phase = 1;
-            for (int k = 0; k < N; ++k) {
-                W_01(k, n) = phase * W_00(k, n);
-                phase *= -1;
+        for (int i = 0; i < N; ++i) {
+            // TODO Do we need to reinit to zero on each new iteration?
+            auto iaf_i = column(iaf, i);
+            int min = std::min(i, N - 1 - i);
+            int total = 2 * min + 1;
+            auto lag_plus =
+                elements(psi, [&](int tau) { return i + (tau - min); }, total);
+            auto lag_minus =
+                elements(psi, [&](int tau) { return i - (tau - min); }, total);
+            auto index = elements(
+                iaf_i, [&](int tau) { return (N + tau - min) % N; }, total);
+            index = lag_plus * conj(lag_minus);
+            for (int j = 0; 2 * j < N; ++j) {
+                iaf_i[2 * j + 1] *= -1;
             }
         }
+        fftw_execute(c2c);
 
-        // W(k+N,n) = (-1)^n W(k,n)
-        auto W_10 = submatrix(f, N, 0, N, N);
-        for (int n = 0; 2 * n < N; ++n) {
-            column(W_10, 2 * n) = column(W_00, 2 * n);
-            column(W_10, 2 * n + 1) = -1 * column(W_00, 2 * n + 1);
-        }
-
-        // W(k+N,n+N) = (-1)^(k+n+N) W(k,n)
-        int phase = (N % 2 == 0) ? 1 : -1;
-        auto W_11 = submatrix(f, N, N, N, N);
-        for (int n = 0; n < N; ++n) {
-            for (int k = 0; k < N; ++k) {
-                W_11(k, n) = f(k, n) * phase;
-                phase *= -1;
-            }
-            phase *= -1;
-        }
+        f += state.lambda[m] * real(iaf);
     }
-
-    fftw_destroy_plan(odd_plan);
-    fftw_destroy_plan(even_plan);
 }
+
 void PhaseSpaceDistribution::husimi_distribution(const SimState& state) {
     using namespace blaze;
 
@@ -213,6 +154,8 @@ ObservableFunctor::ReturnType PhaseSpaceDistribution::compute(
     }
     return f;
 }
+
+PhaseSpaceDistribution::~PhaseSpaceDistribution() { fftw_destroy_plan(c2c); }
 
 Potential::Potential(const Parameters& p){};
 
