7e922989e60b9f61bf85be2bd0d5d50885c14b27
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-06-13 20:11:59 +0200

    use inline instead of static

--- inc/state.h
+++ inc/state.h
@@ -23,7 +23,7 @@ struct SimState {
 
 void operator>>(const SimState& state, Parameters& p);
 
-static decltype(auto) delta_from(const SimState& state) {
+inline decltype(auto) delta_from(const SimState& state) {
     auto psi2 = real(conj(state.psis) % state.psis);
     return psi2 * state.lambda - 1.0;
 }
