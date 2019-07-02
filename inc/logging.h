f78fb78ec484172a04dcceee6f2a4caf0efd7301
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-07-02 16:20:48 +0200

    add inline keyword

--- inc/logging.h
+++ inc/logging.h
@@ -5,7 +5,7 @@
 #include <ostream>
 
 template <typename Iter>
-void print(std::ostream& stream, const Iter& begin, const Iter& end) {
+inline void print(std::ostream& stream, const Iter& begin, const Iter& end) {
     stream << std::setprecision(6);
     for (auto i = begin; i != end; ++i) stream << *i << std::endl;
 }
