ddba3dfeb28fb62626535de104fa047bc654ab10
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-07-05 11:56:44 +0200

    generate deltas

--- run/harmonic_static/generate_osci.py
+++ run/harmonic_static/generate_osci.py
@@ -5,25 +5,26 @@ import matplotlib.pyplot as plt
 L = 20 
 omega = 1;
 N = 1024
-cols = 2
 
-pot = np.empty(N*cols).reshape(N, cols)
-pot[:,0] = np.linspace(0, L, N, endpoint=False)
-pot[:,1] = 0.5 * omega ** 2 *(pot[:,0] - L/2)**2
+x = np.linspace(0, L, N, endpoint=False)
+pot = 0.5 * omega ** 2 *(x - L/2)**2
 
-psi = np.empty(N*cols).reshape(N, cols)
-psi[:,0] = (omega/np.pi)**0.25 * np.sqrt(L)* np.exp(-0.5 * omega * (pot[:,0] - L/4)**2)
-psi[:,1] = 0
-
+# First Wavefunction
+psi = np.exp(-0.5 * omega * (x - L/4)**2)
 # Second Wavefunction
-psi[:,0] += np.flip(psi[:,0]) 
+psi += np.flip(psi)
+# Normalization
+psi /= np.sqrt(L/N * np.sum(psi**2))
+print(L/N * np.sum(psi**2))
+
+delta = L * psi**2 - 1
 
 #Check normalization
-print(L/N * np.sum(psi[:,0]**2))
+print(np.sum(delta))
 
-plt.plot(pot[:,0], pot[:,1])
-plt.plot(pot[:,0], psi[:,0])
+plt.plot(x, pot)
+plt.plot(x, delta)
 plt.show()
 
-np.savetxt('./potential.txt', pot, header="x \t U")
-np.savetxt('./psi.txt', psi, header="Re(psi) \t Im(psi)")
+np.savetxt('./potential.txt', pot)
+np.savetxt('./delta.txt', delta)
--- run/sinwave_collapse/generate_sin.py
+++ run/sinwave_collapse/generate_sin.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 
 # Code Units
 L = 1 
-A = 0.1;
-N = 1024
-cols = 2
+A = 0.1
+N = 2**10
 
 x = np.linspace(0, L, N, endpoint=False)
+dx = x[1] - x[0]
 
-psi = np.empty(N*cols).reshape(N, cols)
-psi[:,0] = np.sqrt(1.0/L * (A * np.sin(2*np.pi/L * x) + 1))
-psi[:,1] = 0
+delta = -A * np.cos(2*np.pi/L * x)
 
-plt.plot(x, psi[:,0])
+print(np.sum(delta))
+
+plt.plot(x, delta)
 plt.show()
 
-np.savetxt('../run/psi_sin.txt', psi, header="Re(psi) \t Im(psi)")
+np.savetxt('./delta.txt', delta)
