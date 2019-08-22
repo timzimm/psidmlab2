2e383c04586329f38c4201af9da0e91b83976374
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-08-22 14:47:07 +0200

    prep for merge

--- run/tophat_collapse/generate_tophat.py
+++ run/tophat_collapse/generate_tophat.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 
 # Code Units
 L = 1
-beta = 128/L
-N = 16384
+beta = 10/L
+Ns = [2**14, 2**15]
 
-for N in np.power(8 * [2], range(9,17)):
-    x = np.linspace(-0.5, 0.5, N, endpoint=False)
-    delta = np.tanh(beta*(x+L/20)) + np.tanh(beta*(L/20-x))
-    delta -= L/N * np.sum(delta)
-    plt.plot(x, delta)
-    plt.show()
-    np.savetxt('./delta_%d.txt' % N, delta)
+for N in Ns:
+    for width in [0.01, 0.02, 0.04, 0.06, 0.08, 0.1]:
+        x = np.linspace(-0.5, 0.5, N, endpoint=False)
+        delta = np.tanh(beta/width*(x+0.5*width*L)) + np.tanh(beta/width*(0.5*width*L-x))
+        delta -= L/N * np.sum(delta)
+        plt.plot(x, delta)
+        plt.show()
+        np.savetxt('./ic/delta_%dkpc_%d.txt' % (width * 1000, N), delta)
