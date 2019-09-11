ce855c3ea2752d32eb7bbc425ae99662466ae19e
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-09-11 19:59:21 +0200

    add imaginary time propagation

---
+++ src/schroedinger/uso_kdk_itp.cc
@@ -0,0 +1,69 @@
+#include "schroedinger/uso_kdk_itp.h"
+#include "cosmology.h"
+#include "parameters.h"
+
+#include <algorithm>
+
+using namespace blaze;
+using namespace std::complex_literals;
+
+namespace Schroedinger {
+
+USO_KDK_ITP::USO_KDK_ITP(const Parameters& p, const SimState& state,
+                         const Cosmology& cosmo_)
+    : DefaultDriver{p},
+      cosmo{cosmo_},
+      pot{PotentialMethod::make(p["Simulation"]["potential"].get<std::string>(),
+                                p)},
+      N{p["Simulation"]["N"].get<int>()},
+      L{p["Simulation"]["L"].get<double>()},
+      a{cosmo.a_of_tau(-1)},
+      k_squared(N),
+      kick(N, state.M),
+      dt_last{-1},
+      stable{p["Simulation"]["integrator"]["stable"].get<bool>()} {
+    // FFTW reorders frequencies. The upper half starts at the most negative
+    // frequency and increases for increasing index.
+    std::iota(k_squared.begin(), k_squared.end(), -N / 2);
+    std::rotate(k_squared.begin(), k_squared.end() - (N + 1) / 2,
+                k_squared.end());
+    k_squared = 4 * M_PI * M_PI / (L * L) * k_squared * k_squared;
+}
+
+void USO_KDK_ITP::step(SimState& state, const double dt) {
+    CCM& psis = state.psis;
+    const double a_next = cosmo.a_of_tau(state.tau + dt);
+
+    const std::complex<double> idt = -1.0i * dt;
+
+    // Set kick operator once for the entire time step
+    if (dt - dt_last != 0) {
+        kick = expand(exp(-0.5 * 0.5i * k_squared * idt), state.M);
+    }
+
+    state.transform(SimState::Representation::Momentum);
+    psis = kick % psis;
+
+    state.transform(SimState::Representation::Position);
+    // Recompute potential
+    pot->solve(state);
+
+    // Set drift operator with intermediate potential
+    auto drift =
+        expand(exp(-1.0 * 0.5i * (a + a_next) * state.V * idt), state.M);
+    psis = drift % psis;
+
+    state.transform(SimState::Representation::Momentum);
+    psis = kick % psis;
+
+    state.tau += dt;
+    a = a_next;
+    dt_last = dt;
+    state.n += 1;
+}
+
+double USO_KDK_ITP::next_dt(const SimState& state) const {
+    return std::min({L * L / (N * N * M_PI), M_PI / (a * max(abs(state.V)))});
+}
+
+}  // namespace Schroedinger
