4a20673ff9191f9945fbda189183c001de8764c3
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-11-27 15:03:02 +0100

    avoid cp

--- src/poisson/gp.cc
+++ src/poisson/gp.cc
@@ -8,10 +8,14 @@ namespace Poisson {
 GP::GP(const Parameters& p) {}
 GP::~GP() = default;
 
-void GP::solve(SimState& state) { solve(state.V, delta_from(state)); }
+void GP::solve(SimState& state) {
+    // No copy
+    state.V = delta_from(state);
+}
 
 void GP::solve(blaze::DynamicVector<double, blaze::columnVector>& V,
                const blaze::DynamicVector<double, blaze::columnVector>& s) {
+    // TODO: Move from const is copy !!!
     V = std::move(s);
 }
 }  // namespace Poisson
