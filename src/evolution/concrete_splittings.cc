9aad9589013b419a247413b9998b49a7704d0fc4
Tim Zimmermann <tt.zimmermann@gmx.de>
2019-12-16 16:08:20 +0100

    add CAP splittings

--- src/evolution/concrete_splittings.cc
+++ src/evolution/concrete_splittings.cc
@@ -1,11 +1,16 @@
 #include "splitting.h"
 
 // Flow Operators
+#include "evolution/cap_poisson_potential_delta.h"
 #include "evolution/interaction_potential_trapezodial.h"
 #include "evolution/kinetic.h"
 
 template class SRKN<Schroedinger::Kinetic,
                     Schroedinger::InteractionPotentialTrapezodial>;
 template class SRKN<Schroedinger::InteractionPotentialTrapezodial,
+                    Schroedinger::Kinetic>;
+template class SRKN<Schroedinger::Kinetic,
+                    Schroedinger::CAPPoissonPotentialDelta>;
+template class SRKN<Schroedinger::CAPPoissonPotentialDelta,
                     Schroedinger::Kinetic>;
 
