cc811e2ed8ef6e2a45bddd392c0f8ee95bd65c01
Tim Zimmermann <tt.zimmermann@gmx.de>
2020-01-14 17:28:53 +0100

    add physical box lengths, round up to next even N and always expand box.Ns to 3D

--- inc/domain.h
+++ inc/domain.h
@@ -7,14 +7,13 @@
 #include <vector>
 
 struct Domain {
-    using interval_t = std::array<double, 2ul>;
-
-    std::vector<int> Ns;          // degrees of freedom per dimension
-    std::vector<interval_t> box;  // box limits as in config (code or physical)
-    std::vector<double> box_lengths;  // box lengths (code units)
-    std::vector<double> dxs;          // dx's (code units)
-    size_t dims;                      // # of dimensions
-    size_t N_total;                   // total # of Ns (N_1 * ... * N_dims)
+    std::vector<int> Ns;  // degrees of freedom per dimension. Always 3D.
+    std::vector<double> box_lengths;     // box lengths (code units)
+    std::vector<double> box_lengths_pc;  // box lengths (pc)
+    std::vector<double> dxs;             // dx's (code units)
+    std::vector<double> dxs_pc;          // dx's (pc)
+    int dims;                            // # of dimensions
+    size_t N_total;                      // total # of Ns (N_1 * ... * N_dims)
 
     Domain(const Parameters& p);
 };
--- src/domain.cc
+++ src/domain.cc
@@ -15,47 +15,73 @@ BOOST_UNITS_STATIC_CONSTANT(parsec, astronomical::parsec_base_unit::unit_type);
 
 Domain::Domain(const Parameters& p)
     : Ns{p["Domain Properties"]["dof"]},
-      box{},
       box_lengths{},
+      box_lengths_pc{},
       dxs{},
-      dims{Ns.size()},
+      dxs_pc{},
+      dims{static_cast<int>(Ns.size())},
       N_total{0} {
+    using interval_t = std::array<double, 2ul>;
+    std::vector<interval_t> box;
     for (const auto& interval : p["Domain Properties"]["limits"]) {
         box.emplace_back(interval);
         box_lengths.emplace_back(abs(box.back()[1] - box.back()[0]));
+        box_lengths_pc.emplace_back(abs(box.back()[1] - box.back()[0]));
     }
     if (box.size() != dims) {
         std::cout << ERRORTAG("Ill-defined box") << std::endl;
         exit(1);
     }
+
+    const double h{p["Cosmology"]["h"]};
+    const double omega_m0{p["Cosmology"]["omega_m0"]};
+    const double mu{p["Cosmology"]["mu"]};
+
+    // Unit conversion is error prone. We rely on boost:units for
+    // that matter to have compile time assurance for correct
+    // dimensions.
+    using phaseV = derived_dimension<length_base_dimension, 2,
+                                     time_base_dimension, -1>::type;
+    const quantity<energy> eV = e_over_m_e * m_e * volt;
+    const quantity<unit<phaseV, si::system>> mu_si =
+        mu * hbar / (1e-22 * eV) * pow<2>(c);
+    const auto H0 = static_cast<quantity<frequency>>(h * 100 * kilo * meter /
+                                                     (second * mega * parsec));
+    const auto timescale = 1.0 / root<2>(1.5 * pow<2>(H0) * omega_m0);
+
+    // Unit Conversion
     if (p["Domain Properties"]["physical_units"].get<bool>()) {
-        const double h{p["Cosmology"]["h"]};
-        const double omega_m0{p["Cosmology"]["omega_m0"]};
-        const double mu{p["Cosmology"]["mu"]};
-
-        // Unit conversion is error prone. We rely on boost:units for that
-        // matter to have compile time assurance for correct dimensions.
-        using phasevolume = derived_dimension<length_base_dimension, 2,
-                                              time_base_dimension, -1>::type;
-
         auto chi_of_x =
             [&](const quantity<astronomical::parsec_base_unit::unit_type> x) {
-                const quantity<energy> eV = e_over_m_e * m_e * volt;
-                const quantity<unit<phasevolume, si::system>> mu_si =
-                    mu * hbar / (1e-22 * eV) * pow<2>(c);
-                const auto H0 = static_cast<quantity<frequency>>(
-                    h * 100 * kilo * meter / (second * mega * parsec));
-                const auto timescale =
-                    1.0 / root<2>(1.5 * pow<2>(H0) * omega_m0);
-
                 return 1.0 / root<2>(mu_si * timescale) *
                        static_cast<quantity<length>>(x);
             };
-        std::transform(box_lengths.begin(), box_lengths.end(),
+        std::transform(box_lengths_pc.begin(), box_lengths_pc.end(),
                        box_lengths.begin(), chi_of_x);
+    } else {
+        auto x_of_chi = [&](const double x) {
+            return root<2>(mu_si * timescale) /
+                   static_cast<quantity<length>>(1.0 * parsec) * x;
+        };
+        std::transform(box_lengths.begin(), box_lengths.end(),
+                       box_lengths_pc.begin(), x_of_chi);
     }
 
-    for (size_t i = 0; i < dims; ++i) dxs[i] = box_lengths[i] / Ns[i];
+    // Grid Point Adjustments
+    // Only allow even numbered grids in each dimension
+    auto next_even = [](int i) {
+        return static_cast<int>(std::round(i / 2.0) * 2.0);
+    };
+    std::transform(Ns.begin(), Ns.end(), Ns.begin(), next_even);
+
+    // Expand to 3D but set number of grid points to 1 in the new dimensions
+    Ns.resize(3, 1);
+
+    // Spatial Resolutions
+    for (size_t i = 0; i < dims; ++i) {
+        dxs[i] = box_lengths[i] / Ns[i];
+        dxs_pc[i] = box_lengths_pc[i] / Ns[i];
+    }
 
     N_total = std::accumulate(Ns.begin(), Ns.end(), 1, std::multiplies<>());
 }
