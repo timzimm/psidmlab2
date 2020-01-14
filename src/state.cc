16e5ee9b9108f09a8b583da90e65240926bda3c0
Tim Zimmermann <tt.zimmermann@gmx.de>
2020-01-14 17:31:01 +0100

    complete rework of state struct to prepare for N-dims

--- src/state.cc
+++ src/state.cc
@@ -1,38 +1,21 @@
 #include "state.h"
-#include "cosmology.h"
-#include "logging.h"
-#include "parameters.h"
 
-#include <functional>
-#include <numeric>
-
-SimState::SimState(const Parameters& p)
-    : n{0},
+SimState::SimState(const Domain& d)
+    : box{d},
+      n{0},
       tau{0},
-      dofs{p["Domain Properties"]["dof"].get<std::vector<int>>()},
-      box{},
-      dims{dofs.size()},
-      N_total{0},
+      V(box.N_total),
+      psi(box.N_total),
       representation{Representation::Position},
       position_to_momentum{nullptr},
       momentum_to_position{nullptr} {
-    for (auto& limit : p["Domain Properties"]["limits"]) {
-        box.emplace_back(limit.get<interval_t>());
-    }
-    if (box.size() != dims) {
-        std::cout << ERRORTAG("Ill-defined box") << std::endl;
-        exit(1);
-    }
-
-    N_total = std::accumulate(dofs.begin(), dofs.end(), 1, std::multiplies<>());
-    psi.resize(N_total);
-    V.resize(N_total);
-
     auto in = reinterpret_cast<fftw_complex*>(psi.data());
-    position_to_momentum = fftw_plan_dft(dofs.size(), dofs.data(), in, in,
-                                         FFTW_FORWARD, FFTW_ESTIMATE);
-    momentum_to_position = fftw_plan_dft(dofs.size(), dofs.data(), in, in,
-                                         FFTW_BACKWARD, FFTW_ESTIMATE);
+
+    // dimension agnostic transformation (config file determines dimension)
+    position_to_momentum.reset(fftw_plan_dft(box.Ns.size(), box.Ns.data(), in,
+                                             in, FFTW_FORWARD, FFTW_ESTIMATE));
+    momentum_to_position.reset(fftw_plan_dft(box.Ns.size(), box.Ns.data(), in,
+                                             in, FFTW_BACKWARD, FFTW_ESTIMATE));
 }
 
 void SimState::transform(const SimState::Representation target) {
@@ -47,22 +30,12 @@ void SimState::transform(const SimState::Representation target) {
     auto in = reinterpret_cast<fftw_complex*>(psi.data());
 
     if (target == Representation::Momentum) {
-        fftw_execute_dft(position_to_momentum, in, in);
-        // FFTW transforms are denormalized
-        psi /= N_total;
+        fftw_execute_dft(position_to_momentum.get(), in, in);
+        // FFTW transforms are unnormalized
+        psi /= box.N_total;
     } else {
-        fftw_execute_dft(momentum_to_position, in, in);
+        fftw_execute_dft(momentum_to_position.get(), in, in);
     }
 
     representation = target;
 };
-
-SimState::~SimState() {
-    fftw_destroy_plan(position_to_momentum);
-    fftw_destroy_plan(momentum_to_position);
-}
-
-void operator>>(const SimState& state, Parameters& p) {
-    p["Simulation"]["N"] = state.psi.size();
-}
-
