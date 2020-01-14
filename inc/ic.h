c6ac6c27f2ca4608253b2e2f6a44eac9c27e8343
Tim Zimmermann <tt.zimmermann@gmx.de>
2020-01-14 17:26:35 +0100

    fill_from_file now treats consecutive cols as real and imag if passed vector has complex element type

--- inc/ic.h
+++ inc/ic.h
@@ -6,7 +6,7 @@
 struct SimState;
 class Cosmology;
 
-enum class ICType { ExternalDelta, ExternalPsi, Powerspectrum };
+enum class ICType { RealImagPsi, PolarPsi, Powerspectrum };
 
 void generate_ic(SimState& state, const Cosmology& cosmo, const Parameters& p);
 
