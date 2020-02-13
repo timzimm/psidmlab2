50474181e781d585a92d71da745dacc2a3afd558
Tim Zimmermann <tt.zimmermann@gmx.de>
2020-02-13 20:22:51 +0100

    use blaze_config to add compiler definitions

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -5,9 +5,8 @@
 
 vcpkg install boost-math
 vcpkg install boost-variant 
-vcpkg install boost-core 
 vcpkg install boost-units 
-vcpkg install fftw3
+vcpkg install fftw3[avx2]
 vcpkg install hdf5
 vcpkg install --head blaze
 vcpkg install nlohmann-json
