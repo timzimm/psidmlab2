2a8d8b17a073f6529a193f43931485cf2fcbcbfb
Tim Zimmermann <tt.zimmermann@gmx.de>
2020-02-25 15:06:12 +0100

    bump SMP threshold, fix switch typo

--- src/fftw.cc
+++ src/fftw.cc
@@ -1,5 +1,6 @@
 #include "fftw.h"
-#ifdef PSIMDLAB_SMP
+
+#ifdef PSIDMLAB_SMP
 #include <omp.h>
 #endif
 
@@ -11,8 +12,8 @@ fftw_plan_ptr make_fftw_plan_dft(int N, const fftw_complex* vin,
     auto in = const_cast<fftw_complex*>(vin);
     auto out = const_cast<fftw_complex*>(vout);
 
-#ifdef PSIMDLAB_SMP
-    const int max_N_per_thread = 1 << 14;
+#ifdef PSIDMLAB_SMP
+    const int max_N_per_thread = 1 << 15;
     const int nthreads =
         std::min({N / max_N_per_thread, omp_get_max_threads()});
     fftw_plan_with_nthreads(nthreads);
@@ -26,8 +27,8 @@ fftw_plan_ptr make_fftw_plan_dft_r2c(int N, const double* vin,
     auto in = const_cast<double*>(vin);
     auto out = const_cast<fftw_complex*>(vout);
 
-#ifdef PSIMDLAB_SMP
-    const int max_N_per_thread = 1 << 14;
+#ifdef PSIDMLAB_SMP
+    const int max_N_per_thread = 1 << 15;
     const int nthreads =
         std::min({N / max_N_per_thread, omp_get_max_threads()});
     fftw_plan_with_nthreads(nthreads);
@@ -42,8 +43,8 @@ fftw_plan_ptr make_fftw_plan_dft_c2r(int N, const fftw_complex* vin,
     auto in = const_cast<fftw_complex*>(vin);
     auto out = const_cast<double*>(vout);
 
-#ifdef PSIMDLAB_SMP
-    const int max_N_per_thread = 1 << 14;
+#ifdef PSIDMLAB_SMP
+    const int max_N_per_thread = 1 << 15;
     const int nthreads =
         std::min({N / max_N_per_thread, omp_get_max_threads()});
     fftw_plan_with_nthreads(nthreads);
