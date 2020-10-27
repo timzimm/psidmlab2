a9405358f43136a5a90d888a227cb226bbc95245
Tim Zimmermann <tt.zimmermann@gmx.de>
2020-10-27 21:07:06 +0100

    add plots.py

---
+++ python/plots.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import itertools
+import seaborn as sns
+def set_size(fraction=1, subplots=(1, 1)):
+    """ Set figure dimensions to avoid scaling in LaTeX.
+
+    Parameters
+    ----------
+    fraction: float, optional
+            Fraction of the width which you wish the figure to occupy
+    subplots: array-like, optional
+            The number of rows and columns of subplots.
+
+    Returns
+    -------
+    fig_dim: tuple
+            Dimensions of figure in inches
+    """
+    # Width of figure (in pts)
+    # fig_width_pt = 426.79134 * fraction
+    fig_width_pt = 468 * fraction
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    golden_ratio = (5**.5 - 1) / 2
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
+
+    fig_dim = (fig_width_in, fig_height_in)
+
+    return fig_dim
+
+def savefig(fig, filename, dpi=300):
+    fig.savefig(f'{filename}.pdf', format='pdf', dpi=dpi, bbox_inches='tight')
+    # fig.savefig(f'{filename}.pgf', format='pgf', dpi=dpi, bbox_inches='tight')
+
